ca5e269bbfff50e8318125d223c845b197ae2958
Charles Ponti <cj@ponti.io>
2020-11-08 13:57:52 -0500

    (style) Use new green color

--- src/components/Cards/Recovered.jsx
+++ src/components/Cards/Recovered.jsx
@@ -9,7 +9,7 @@ import CountUpTo from '../CountUpTo'
 export default function Recovered ({ recovered }) {
   return (
     <>
-      <FontAwesomeIcon icon={faVirusSlash} size="4x" style={{ marginBottom: '1rem', color: "green" }}/>
+      <FontAwesomeIcon icon={faVirusSlash} size="4x" style={{ marginBottom: '1rem', color: "#2ec72e" }}/>
       <Title title="Recovered"></Title>
       <CountUpTo value={recovered} />
     </>

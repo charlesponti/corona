fc7b5ace120ec9e3678f3da6d7f0a2525dc01f8f
Charles Ponti <cj@ponti.io>
2020-11-08 00:10:31 -0500

    (style) simplify percentage deaths

--- src/data/convert.js
+++ src/data/convert.js
@@ -1,9 +1,12 @@
+/* global __dirname */
+// Data from: https://www.cdc.gov/nchs/nvss/vsrr/covid_weekly/index.htm
+
 const fs = require('fs')
 const path = require('path')
 const csv = require('csvtojson');
 
 (async () => {
-  const csvFilePath = path.resolve(__dirname, './deaths.csv')
+  const csvFilePath = path.resolve(__dirname, './deaths-20-11-04.csv')
   const deaths = await csv().fromFile(csvFilePath)
   fs.writeFileSync(path.resolve(__dirname, './deaths.json'), JSON.stringify(deaths))
 })()

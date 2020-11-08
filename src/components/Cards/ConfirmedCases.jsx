a7f892e4c0811b61e8501f7e8a5df51615ad2e21
Charles Ponti <cj@ponti.io>
2020-11-07 22:50:51 -0500

    (style) Simplify style of cards

--- src/components/Cards/ConfirmedCases.jsx
+++ src/components/Cards/ConfirmedCases.jsx
@@ -1,24 +1,18 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Card from '@material-ui/core/Card'
-import CardContent from '@material-ui/core/CardContent'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faVirus } from '@fortawesome/free-solid-svg-icons'
 
 import Title from '../Title'
 import CountUpTo from '../CountUpTo'
-import cx from 'classnames'
-import styles from './Cards.module.css'
 
 export default function ConfirmedCases ({ confirmed }) {
   return (
-    <Card className={cx(styles.card, styles.confirmed)}>
-      <CardContent>
-        <FontAwesomeIcon icon={faVirus} size="4x" style={{ marginBottom: '1rem' }}/>
-        <Title title="Confirmed Cases" />
-        <CountUpTo value={confirmed} />
-      </CardContent>
-    </Card>
+    <>
+      <FontAwesomeIcon icon={faVirus} size="4x" style={{ marginBottom: '1rem', color: "purple" }}/>
+      <Title title="Confirmed Cases" />
+      <CountUpTo value={confirmed} />
+    </>
   )
 }
 
